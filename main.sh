6621a9a1e05f0137b35571ecd9de909e211c7e3c
NokoDev <demozflava@gmail.com>
2022-06-20 22:38:56 +0200

    Added the create vpc line, including the query to extract the id from the response

--- main.sh
+++ main.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-VPC_NAME=$1
-VPC_REGION=$2
-CIDR=$3
+#VPC_NAME=$1
+#VPC_REGION=$2
+#CIDR=$3
 
 
 function makevpc () {
-    vpc_id=$(aws ec2 create-vpc --cidr-block $CIDR )
+    vpc_id=$(aws ec2 create-vpc --cidr-block $1 --region $2 --query "Vpc.VpcId" --output text)
+    sleep 5
+    echo $vpc_id
 }
+
+makevpc 10.0.0.0/16 us-east-2
