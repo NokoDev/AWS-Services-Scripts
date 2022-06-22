d4f97f4b1cb00190a3086c25391f22a492f178c8
NokoDev <demozflava@gmail.com>
2022-06-22 20:39:27 +0200

    Added the create subnet function

--- main.sh
+++ main.sh
@@ -5,10 +5,20 @@
 #CIDR=$3
 
 
-function makevpc () {
+function createVpc () {
     vpc_id=$(aws ec2 create-vpc --cidr-block $1 --region $2 --query "Vpc.VpcId" --output text)
     sleep 5
     echo $vpc_id
 }
 
-makevpc 10.0.0.0/16 us-east-2
+function createSubnet () {
+    subnet_id=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block $1 --query "Subnet.SubnetId" --output text)
+    
+    echo $subnet_id
+
+}
+
+
+
+createVpc 10.0.0.0/16 us-east-2
+createSubnet 10.0.0.0/24
